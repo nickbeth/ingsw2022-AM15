f3d8456796e71f172baf0a43d54ad2efbc03ed20
lynxnb <niccolo.betto@gmail.com>
2022-03-01 16:31:17 +0100

    Initial project upload

---
+++ src/main/java/it/polimi/ingsw/eriantys/App.java
@@ -0,0 +1,10 @@
+package it.polimi.ingsw.eriantys;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
