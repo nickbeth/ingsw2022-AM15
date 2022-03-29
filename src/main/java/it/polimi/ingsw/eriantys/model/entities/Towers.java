d0b1a9862c1bbfd1fa9db7c357daa3d7eecde49b
gabriele-caliandro <gabrielecaliandro2001@gmail.com>
2022-03-29 16:45:22 +0200

    Creating basic obj classes: Towers, Cloud, Dashboard, Island + added maven-assembly plugin and mockito dependency in pom

---
+++ src/main/java/it/polimi/ingsw/eriantys/model/entities/Towers.java
@@ -0,0 +1,8 @@
+package it.polimi.ingsw.eriantys.model.entities;
+
+import it.polimi.ingsw.eriantys.model.entities.enums.TowerColor;
+
+public class Towers {
+  public int count;
+  public TowerColor color;
+}
