8280a6c75200fb049dff96eaac116b338fe364a3
gabriele-caliandro <gabrielecaliandro2001@gmail.com>
2022-04-05 11:11:35 +0200

    Created ActionInvoker and abstract PlayerAction

--- src/main/java/it/polimi/ingsw/eriantys/model/ActionInvoker.java
+++ src/main/java/it/polimi/ingsw/eriantys/model/ActionInvoker.java
@@ -1,2 +1,22 @@
-package it.polimi.ingsw.eriantys.model;public class ActionInvoker {
+package it.polimi.ingsw.eriantys.model;
+
+import java.util.*;
+
+public class ActionInvoker {
+  private List<PlayerAction> gameActions;
+  private GameState gameState;
+
+  private ActionInvoker(GameState gameState) {
+    gameActions = new ArrayList<>();
+    this.gameState = gameState;
+  }
+
+  public boolean executeAction(PlayerAction action) {
+    if (action.isValid(gameState)) {
+      action.apply(gameState);
+      gameActions.add(action);
+      return true;
+    }
+    return false;
+  }
 }
--- src/main/java/it/polimi/ingsw/eriantys/model/PlayerAction.java
+++ src/main/java/it/polimi/ingsw/eriantys/model/PlayerAction.java
@@ -1,2 +1,8 @@
-package it.polimi.ingsw.eriantys.model;public class PlayerAction {
+package it.polimi.ingsw.eriantys.model;
+
+public abstract class PlayerAction {
+  protected String playerNickname;
+
+  public abstract void apply(GameState gameState);
+  public abstract boolean isValid(GameState gameState);
 }
