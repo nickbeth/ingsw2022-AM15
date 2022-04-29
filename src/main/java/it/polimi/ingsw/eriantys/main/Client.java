6064c3bc4b48f98c8a18a1f5dbcbabbeb78fa182
lynxnb <niccolo.betto@gmail.com>
2022-04-29 18:27:41 +0200

    Create client and server entrypoints and configure jar assembly

---
+++ src/main/java/it/polimi/ingsw/eriantys/main/Client.java
@@ -0,0 +1,7 @@
+package it.polimi.ingsw.eriantys.main;
+
+public class Client {
+  public static void main(String[] args) {
+    System.out.println("Client");
+  }
+}
---
+++ src/main/java/it/polimi/ingsw/eriantys/main/Server.java
@@ -0,0 +1,7 @@
+package it.polimi.ingsw.eriantys.main;
+
+public class Server {
+  public static void main(String[] args) {
+    System.out.println("Server");
+  }
+}
