2dab3e379c4e2a12e3daad43c70a8c92b19dc44b
lynxnb <niccolo.betto@gmail.com>
2022-05-05 15:34:23 +0200

    Introduce a `View` class
    The `View` class will handle drawing a model entity to the given output stream. The `ViewGroup` class is a wrapper around a list of `View`s which allows to add/remove views from it and draw them all at once.

---
+++ src/main/java/it/polimi/ingsw/eriantys/cli/View.java
@@ -0,0 +1,17 @@
+package it.polimi.ingsw.eriantys.cli;
+
+import java.io.IOException;
+import java.io.PrintStream;
+
+/**
+ * Base class for all views.
+ */
+public abstract class View {
+  /**
+   * Draws this view to an output stream.
+   *
+   * @param o The output stream which the view will write to.
+   * @throws IOException If an I/O error occurs on the output stream.
+   */
+  abstract public void draw(PrintStream o) throws IOException;
+}
---
+++ src/main/java/it/polimi/ingsw/eriantys/cli/ViewGroup.java
@@ -0,0 +1,41 @@
+package it.polimi.ingsw.eriantys.cli;
+
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * A View which contains multiple views. When this view is drawn, it will call the draw method of each contained view.
+ */
+public class ViewGroup extends View {
+  private final List<View> viewList = new LinkedList<>();
+
+  /**
+   * Adds a view to this group.
+   *
+   * @param view The view to add to this group.
+   */
+  public void addView(View view) {
+    viewList.add(view);
+  }
+
+  /**
+   * Removes a view from this group.
+   *
+   * @param view The view to remove from this group.
+   * @return True if the view was removed, false otherwise.
+   */
+  public boolean removeView(View view) {
+    return viewList.remove(view);
+  }
+
+  /**
+   * Draws all views in this group.
+   */
+  public void draw(PrintStream o) throws IOException {
+    for (View view : viewList) {
+      view.draw(o);
+    }
+  }
+}
