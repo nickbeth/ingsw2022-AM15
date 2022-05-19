397779e2e1ccb0f04e1de46b8d786d91fcfb8d66
lynxnb <niccolo.betto@gmail.com>
2022-05-19 15:33:24 +0200

    Rework client app entrypoint

--- src/main/java/it/polimi/ingsw/eriantys/client/ClientApp.java
+++ src/main/java/it/polimi/ingsw/eriantys/client/ClientApp.java
@@ -1,119 +1,29 @@
 package it.polimi.ingsw.eriantys.client;
 
-import it.polimi.ingsw.eriantys.controller.CliController;
 import it.polimi.ingsw.eriantys.controller.Controller;
-import it.polimi.ingsw.eriantys.controller.menus.Menu;
-import it.polimi.ingsw.eriantys.controller.menus.ParamBuilder;
-import it.polimi.ingsw.eriantys.controller.menus.planning.MenuPickAssistantCard;
-import it.polimi.ingsw.eriantys.model.actions.GameAction;
-import it.polimi.ingsw.eriantys.model.actions.PickAssistantCard;
-import it.polimi.ingsw.eriantys.network.*;
+import it.polimi.ingsw.eriantys.network.Client;
+import it.polimi.ingsw.eriantys.network.MessageQueueEntry;
 
-import java.io.IOException;
-import java.util.Scanner;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 
 public class ClientApp {
-  private String address;
-  private int port = Client.DEFAULT_PORT;
-  
-  private final Scanner scanner;
+  private final boolean isGui;
+
   private final Client networkClient;
-  
-  private String nickname;
-  private Controller controller;
-  
-  public ClientApp() {
-    this.scanner = new Scanner(System.in);
+  private final MessageHandler messageHandler;
+
+  private final Controller controller;
+
+  public ClientApp(boolean isGui) {
+    this.isGui = isGui;
     BlockingQueue<MessageQueueEntry> messageQueue = new LinkedBlockingQueue<>();
     this.networkClient = new Client(messageQueue);
+    this.messageHandler = new MessageHandler(messageQueue);
+    this.controller = Controller.create(isGui, networkClient, messageHandler);
   }
-  
+
   public void run() {
-    while (true) {
-      try {
-        readServerAddress();
-        networkClient.connect(address, port);
-        System.out.print("Enter your nickname: ");
-        String input = scanner.nextLine();
-        if (!input.isEmpty())
-          nickname = input;
-        new Thread(networkClient, "sock").start();
-        break;
-      } catch (IOException e) {
-        System.out.println("Failed to connect to the server: '" + e.getMessage() + "'");
-      }
-    }
-    sendCommands();
+    controller.run();
   }
-  
-  /**
-   * Asks the user for server's address and port
-   */
-  private void readServerAddress() {
-    System.out.print("Enter the IP address of the server (default: localhost): ");
-    String input = scanner.nextLine();
-    if (!input.isEmpty())
-      address = input;
-    
-    System.out.print("Enter the port the server is running on (default: 1234): ");
-    
-    while (true) {
-      input = scanner.nextLine();
-      try {
-        if (!input.isEmpty())
-          port = Integer.parseInt(input);
-        break;
-      } catch (NumberFormatException e) {
-        System.out.println("Invalid port, try again");
-      }
-    }
-  }
-  
-  /**
-   *
-   */
-  private void sendCommands() {
-    String input;
-    Menu menu = null;
-    while (true) {
-      try {
-//        printMenu();
-        input = scanner.nextLine();
-        
-        switch (Integer.parseInt(input)) {
-          case 1 -> {
-            if (menu == null)
-              menu = new MenuPickAssistantCard(controller.getGameState(), nickname, controller);
-            else
-              menu = menu.nextMenu();
-            menu.makeChoice(new ParamBuilder());
-//            GameAction action = new PickAssistantCard(1);
-//            Message message = new Message.Builder().type(MessageType.GAMEDATA).action(action).build();
-//            networkClient.send(message);
-          }
-          case 0 -> {
-            networkClient.close();
-            return;
-          }
-          default -> throw new NumberFormatException();
-        }
-      } catch (NumberFormatException e) {
-        System.out.println("Invalid choice, please try again");
-      } catch (Exception e) {
-        e.printStackTrace();
-      }
-    }
-  }
-  
-  // Unnecessary menu
-//  private void printMenu() {
-//    StringBuilder ss = new StringBuilder();
-//
-//    ss.append("1. Invia un message contenente una `PickAssistantCard` action\n")
-//            .append("0. Esci\n");
-//
-//    System.out.print(ss);
-//  }
 }
--- src/main/java/it/polimi/ingsw/eriantys/client/Main.java
+++ src/main/java/it/polimi/ingsw/eriantys/client/Main.java
@@ -5,17 +5,17 @@ import org.tinylog.Logger;
 public class Main {
   public static void main(String[] args) {
     System.out.println("Eriantys | 1.0 | AM15 | Client");
-    boolean isGui;
+    boolean isGui = true;
 
     // Arguments parsing
     for (String arg : args) {
       switch (arg) {
-        case "-g", "--gui" -> isGui = true;
+        case "-c", "--cli" -> isGui = false;
         default -> Logger.warn("Unknown command line argument: {}", arg);
       }
     }
 
-    ClientApp clientApp = new ClientApp();
+    ClientApp clientApp = new ClientApp(isGui);
     clientApp.run();
   }
 }
