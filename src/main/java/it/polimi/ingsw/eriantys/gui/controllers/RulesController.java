9d35e65235fb50292b4e5fefdb1b41d81d3faf4b
Gabriele Caliandro <gabriele.caliandro@mail.polimi.it>
2022-05-24 01:13:42 +0200

    Created navigation buttons in Rules scene

--- src/main/java/it/polimi/ingsw/eriantys/gui/controllers/RulesController.java
+++ src/main/java/it/polimi/ingsw/eriantys/gui/controllers/RulesController.java
@@ -4,23 +4,53 @@ import it.polimi.ingsw.eriantys.gui.SceneEnum;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 import java.beans.PropertyChangeEvent;
+import java.io.InputStream;
+import java.util.Objects;
 
 public class RulesController extends FXMLController {
   @FXML
-  private Button backButton;
-
+  private ImageView view;
+  @FXML
+  private Button nextPage;
+  @FXML
+  private Button previousPage;
+  private int currPage = 1;
+  
   /**
    * Sets scene to Menu
    */
   @FXML
-  public void backButtonAction(ActionEvent actionEvent) {
+  private void backButtonAction(ActionEvent actionEvent) {
+    currPage = 1;
     gui.setScene(SceneEnum.MENU);
   }
-
+  
+  @FXML
+  public void goNextPage(ActionEvent actionEvent) {
+    nextPage.setVisible(currPage < 6);
+    currPage++;
+    previousPage.setVisible(true);
+    view.setImage(getImage());
+  }
+  
+  @FXML
+  private void goPreviousPage(ActionEvent actionEvent) {
+    previousPage.setVisible(currPage > 2);
+    currPage--;
+    nextPage.setVisible(true);
+    view.setImage(getImage());
+  }
+  
+  private Image getImage() {
+    InputStream stream = getClass().getResourceAsStream("/assets/rules/Eriantys_rules-" + currPage + ".png");
+    return new Image(Objects.requireNonNull(stream));
+  }
+  
   @Override
   public void propertyChange(PropertyChangeEvent evt) {
   }
-
 }
