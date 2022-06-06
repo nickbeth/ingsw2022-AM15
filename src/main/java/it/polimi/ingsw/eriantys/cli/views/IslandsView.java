211271268d313fc0b9ca8ed1580fd2f1061aab39
Gabriele Caliandro <gabriele.caliandro@mail.polimi.it>
2022-06-06 21:19:50 +0200

    Cli View: embelished Islands view

--- src/main/java/it/polimi/ingsw/eriantys/cli/views/IslandsView.java
+++ src/main/java/it/polimi/ingsw/eriantys/cli/views/IslandsView.java
@@ -1,22 +1,17 @@
 package it.polimi.ingsw.eriantys.cli.views;
 
 import it.polimi.ingsw.eriantys.model.entities.Island;
-import it.polimi.ingsw.eriantys.model.enums.HouseColor;
-import it.polimi.ingsw.eriantys.model.enums.TowerColor;
 
 import java.io.PrintStream;
-import java.text.MessageFormat;
 import java.util.List;
-import java.util.Optional;
 
-import static it.polimi.ingsw.eriantys.cli.utils.BoxSymbols.VERTICAL;
 import static it.polimi.ingsw.eriantys.cli.utils.PrintUtils.*;
 import static it.polimi.ingsw.eriantys.cli.views.IslandView.drawIsland;
-import static java.lang.Enum.valueOf;
 
 public class IslandsView extends View {
-  private List<Island> islands;
-  private int motherNaturePos;
+  private final List<Island> islands;
+  private final int motherNaturePos;
+  private final int maxColumns = 2;
 
   public IslandsView(List<Island> islands, int motherNaturePos) {
     this.islands = islands;
@@ -28,7 +23,7 @@ public class IslandsView extends View {
    */
   @Override
   public void draw(PrintStream o) {
-    int maxColumns = 2;
+    StringBuilder stringBuilder = new StringBuilder();
 
     // Populate the matrix
     int rows = drawIsland(islands.get(0), 0, 0).split(System.lineSeparator()).length;
@@ -43,11 +38,12 @@ public class IslandsView extends View {
 
       // Build the stripes
       for (int islandIndex = progression; islandIndex < islands.size() / 2; islandIndex++) {
+//      for (int islandIndex = progression; islandIndex < (progression / 2)  + maxColumns; islandIndex++) {
         for (int row = 0; row < rows; row++) {
-          o
+          stringBuilder
               .append(matrix[islandIndex][row])
               .append(matrix[matrix.length - islandIndex - 1][row]);
-          o.append(System.lineSeparator());
+          stringBuilder.append(System.lineSeparator());
         }
         progression += maxColumns;
       }
@@ -56,18 +52,36 @@ public class IslandsView extends View {
     // Build last stripes
     if (islands.size() % 2 == 1) {
       for (int row = 0; row < rows; row++) {
-        o
-            .append(centred(matrix[(int) Math.floor((double) islands.size() / 2)][row]))
+        stringBuilder
+            // Passing middle island
+            .append(centredIsland(matrix[islands.size() / 2][row]))
             .append(System.lineSeparator());
       }
     }
+
+
+    // Title of the section
+    o.append(centredTitle("ISLANDS")).append(System.lineSeparator());
+
+    // Write the content
+    o.append(stringBuilder);
+
+    // Writes a "-" separator
+    o.append(centredTitle("")).append(System.lineSeparator());
   }
 
-  private String centred(String row) {
-    int baseRowLenght = "╭────────────────────────────────╮".length();
-    int nPadding = baseRowLenght / 2;
+  private String centredIsland(String row) {
+    int baseRowLength = "╭────────────────────────────────╮".length();
+    int nPadding = baseRowLength / 2;
 
     return PADDING.repeat(nPadding) + row;
+  }
+
+  private String centredTitle(String row) {
+    int baseRowLength = "╭────────────────────────────────╮".length();
+    int nPadding = baseRowLength - (int) (Math.floor((double) row.length() / 2));
+
+    return "-".repeat(nPadding) + row + "-".repeat(nPadding);
   }
 }
 
