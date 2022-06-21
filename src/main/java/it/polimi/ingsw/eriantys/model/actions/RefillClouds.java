8eb7bf289b6becf5c026553efebc54dfe1dd4e07
lynxnb <niccolo.betto@gmail.com>
2022-06-22 01:26:45 +0200

    RefillClouds: add alternative constructor for clouds initialisation

--- src/main/java/it/polimi/ingsw/eriantys/model/actions/RefillClouds.java
+++ src/main/java/it/polimi/ingsw/eriantys/model/actions/RefillClouds.java
@@ -5,11 +5,31 @@ import it.polimi.ingsw.eriantys.model.GameState;
 import it.polimi.ingsw.eriantys.model.entities.StudentBag;
 import it.polimi.ingsw.eriantys.model.entities.Students;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class RefillClouds extends GameAction {
   private final List<Students> clouds;
 
+  public RefillClouds(GameState gameState) {
+    List<Students> cloudsStudents = new ArrayList<>();
+    Students temp = new Students();
+    StudentBag currentBag = gameState.getPlayingField().getStudentBag();
+
+    // Populate clouds with random students from bag
+    for (int cloudIter = 0; cloudIter < gameState.getRuleBook().cloudCount; cloudIter++) {
+      for (int cloudSizeIter = 0; cloudSizeIter < gameState.getRuleBook().playableStudentCount; cloudSizeIter++) {
+        temp.addStudent(currentBag.takeRandomStudent());
+      }
+      cloudsStudents.add(new Students(temp));
+      temp = new Students(); // clear temp
+    }
+    clouds = cloudsStudents;
+  }
+
+  /**
+   * @deprecated {@link #RefillClouds(GameState)} should be used instead.
+   */
   public RefillClouds(List<Students> s) {
     this.clouds = s;
   }
