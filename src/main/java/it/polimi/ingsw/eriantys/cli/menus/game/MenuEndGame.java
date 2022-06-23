19821708dd07ac4fb9dd01a9ef3d1c2d76169658
Gabriele Caliandro <gabriele.caliandro@mail.polimi.it>
2022-06-23 15:29:41 +0200

    Cli: create MenuEndGame

--- src/main/java/it/polimi/ingsw/eriantys/cli/menus/game/MenuEndGame.java
+++ src/main/java/it/polimi/ingsw/eriantys/cli/menus/game/MenuEndGame.java
@@ -1,11 +1,16 @@
 package it.polimi.ingsw.eriantys.cli.menus.game;
 
 import it.polimi.ingsw.eriantys.cli.menus.MenuEnum;
+import it.polimi.ingsw.eriantys.cli.views.View;
+import it.polimi.ingsw.eriantys.cli.views.ViewGroup;
+
+import static it.polimi.ingsw.eriantys.cli.utils.PrintUtils.colored;
+import static it.polimi.ingsw.eriantys.model.enums.HouseColor.GREEN;
 
 /**
  * End game display
  */
-public class MenuEndGame extends MenuGame{
+public class MenuEndGame extends MenuGame {
   public MenuEndGame() {
     super();
   }
@@ -16,6 +21,27 @@ public class MenuEndGame extends MenuGame{
 
   @Override
   public MenuEnum show() {
-    return null;
+    out.println();
+    out.println("Game ended");
+    View endScreen = new ViewGroup()
+        .addView(islandsView)
+        .addView(dashboardsView)
+        .addView(playersView);
+
+    endScreen.draw(out);
+
+    StringBuilder result = new StringBuilder();
+    game().getWinner().ifPresentOrElse(
+        winner -> result
+            .append(colored("TEAM ", GREEN))
+            .append(colored(winner.toString(), winner))
+            .append(" HAS WON THE GAME"),
+        () ->
+            result.append("IT'S A TIE")
+    );
+
+    out.print(result);
+
+    return MenuEnum.CREATE_OR_JOIN;
   }
 }
