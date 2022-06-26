1ec2404524be3a8124313f3be7ec0304584c651b
zibas <milo.brontesi@gmail.com>
2022-06-26 18:35:19 +0200

    Gui: added allowed amounts to prompt texts

--- src/main/java/it/polimi/ingsw/eriantys/gui/controllers/section_handlers/PromptTextHandler.java
+++ src/main/java/it/polimi/ingsw/eriantys/gui/controllers/section_handlers/PromptTextHandler.java
@@ -2,6 +2,8 @@ package it.polimi.ingsw.eriantys.gui.controllers.section_handlers;
 
 import it.polimi.ingsw.eriantys.controller.Controller;
 import it.polimi.ingsw.eriantys.model.GameState;
+import it.polimi.ingsw.eriantys.model.RuleBook;
+import it.polimi.ingsw.eriantys.model.entities.Player;
 import it.polimi.ingsw.eriantys.model.entities.character_cards.CharacterCard;
 import it.polimi.ingsw.eriantys.model.enums.GameMode;
 import it.polimi.ingsw.eriantys.model.enums.GamePhase;
@@ -31,7 +33,6 @@ public class PromptTextHandler extends SectionHandler {
     TurnPhase turnPhase = gameState.getTurnPhase();
     String currentPlayer = gameState.getCurrentPlayer().getNickname();
     CharacterCard playedCard = gameState.getPlayingField().getPlayedCharacterCard();
-
     String expertAddon = "";
     if (Controller.get().getNickname().equals(currentPlayer))
       currentPlayer = "YOUR";
@@ -45,13 +46,25 @@ public class PromptTextHandler extends SectionHandler {
       return "IT'S " + currentPlayer.toUpperCase() + " TURN TO PLAY AN ASSISTANT CARD";
     if (gamePhase == GamePhase.ACTION) {
       if (turnPhase == TurnPhase.PLACING)
-        return "IT'S " + currentPlayer.toUpperCase() + " TURN TO PLACE STUDENTS" + expertAddon;
-      if (turnPhase == TurnPhase.MOVING)
-        return "IT'S " + currentPlayer.toUpperCase() + " TURN TO MOVE MOTHER NATURE" + expertAddon;
+        return "IT'S " + currentPlayer.toUpperCase() + " TURN TO PLACE " + studentsLeftToMove() + " STUDENTS" + expertAddon;
+      if (turnPhase == TurnPhase.MOVING) {
+        int maxMovement = gameState.getCurrentPlayer().getMaxMovement();
+        return "IT'S " + currentPlayer.toUpperCase() + " TURN TO MOVE MOTHER NATURE " + "(MAX: " + maxMovement + " MOVES) " + expertAddon;
+      }
       if (turnPhase == TurnPhase.PICKING)
         return "IT'S " + currentPlayer.toUpperCase() + " TURN TO PICK A CLOUD";
     }
 
     return "YOUR PROMPT GOES HERE";
   }
+
+  private int studentsLeftToMove() {
+    RuleBook rules = Controller.get().getGameState().getRuleBook();
+    Player currentPlayer = Controller.get().getGameState().getCurrentPlayer();
+    int myCount = currentPlayer.getDashboard().getEntrance().getCount();
+    int finalCount = rules.entranceSize - rules.playableStudentCount;
+    return myCount - finalCount;
+  }
+
+
 }
